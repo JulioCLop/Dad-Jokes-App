6a86fb7a5aa0d5936eba4092201a172abd823935
Julio Lopez <joeyclopez88#gmail.com>
2021-07-14 16:48:30 -0700

    added css to the up down arrows + added the emoji that will be dynamic

--- src/components/joke.component.jsx
+++ src/components/joke.component.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import './joke.style.css';
 
 
 class Joke extends Component{
@@ -8,11 +8,14 @@ class Joke extends Component{
             <div className="joke">
                 <div className="joke-button">
                     <i className="fas fa-arrow-up" onClick={this.props.upvote}></i>
-                    <span>{ this.props.votes}</span>
+                    <span className="joke-votes">{ this.props.votes}</span>
                     <i className="fas fa-arrow-down" onClick={this.props.downvote}></i>
                 </div>
                 <div className="joke-text">
                     {this.props.text}
+                </div>
+                <div className="joke-smiley">
+                    <i class="em em-rolling_on_the_floor_laughing"></i>
                 </div>
             </div>
         )
